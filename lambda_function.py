9c609c1f51b81de61e54ee54aa3382dc28155c03
MihitManoj <mihit.manojm@gmail.com>
2026-09-14 22:57:25 +0530

    Fix expense and budget API

--- lambda_function.py
+++ lambda_function.py
@@ -113,6 +113,16 @@ def lambda_handler(event, context):
 
         table.put_item(Item=expense)
 
+        return {
+        "statusCode": 201,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "message": "Expense added successfully",
+            "expense": expense
+        }, default=str)
+    }
         # -------------------------
         # Check user's budget
         # -------------------------
